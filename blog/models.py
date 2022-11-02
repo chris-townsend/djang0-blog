ec6f851555914b3cc44fbfb2d2d91a79ef9ae3f2
chris-townsend <chris-townsend@live.co.uk>
2022-11-02 13:28:41 +0000

    Add templates and link

--- blog/models.py
+++ blog/models.py
@@ -5,10 +5,12 @@ from cloudinary.models import CloudinaryField
 
 STATUS = ((0, 'Draft'), (1, 'Published'))
 
+
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='blog_posts')
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
     featured_image = CloudinaryField('image', default='placeholder')
@@ -21,26 +23,28 @@ class Post(models.Model):
 class Meta:
     ordering = ['-created_on']
 
+
 def __str__(self):
     return self.title
+
 
 def number_of_likes(self):
     return self.likes.count()
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name='comments')
     name = models.CharField(max_length=80)
     email = models.EmailField()
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
 
+
 class Meta:
-     ordering = ['created_on']
+    ordering = ['created_on']
+
 
 def __str__(self):
     return f"Comment {self.body} by {self.name}"
-
-
-
--- codestar/settings.py
+++ codestar/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cloudinary',
+    'django_summernote',
     'blog',
 ]
 
