55b20d4154a60ed22c26f5b902d8a0b4a3bfe746
chris-townsend <chris-townsend@live.co.uk>
2022-11-03 13:03:02 +0000

    Add comment functionality

--- codestar/settings.py
+++ codestar/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cloudinary',
+    'crispy_forms',
     'django_summernote',
     'blog',
 ]
@@ -56,6 +57,8 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
