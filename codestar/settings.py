f82ea8839780822d302ecd3ed9849cd1683c7e22
chris-townsend <chris-townsend@live.co.uk>
2022-11-03 11:20:34 +0000

    Install django-allauth

--- codestar/settings.py
+++ codestar/settings.py
@@ -40,6 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'cloudinary_storage',
     'django.contrib.messages',
     'django.contrib.staticfiles',
@@ -47,6 +51,11 @@ INSTALLED_APPS = [
     'django_summernote',
     'blog',
 ]
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
